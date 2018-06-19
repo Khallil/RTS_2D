529f0dadebe0a1861553cf56cf4353aa85fa5e9b
= <khallil.doudou@epitech.eu>
2018-06-19 15:37:54 +0800

    add folders

---
+++ Starcraft2D/AppDelegate.swift
@@ -0,0 +1,9 @@
+/// Copyright (c) 2017 Razeware LLC
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+}
+
---
+++ Starcraft2D/Unit.swift
@@ -0,0 +1,60 @@
+/// Copyright (c) 2018 Razeware LLC
+
+import SpriteKit
+
+class Unit: SKSpriteNode{
+  // init le spritNode
+  let life: Int
+  let cost: Int
+  var hb: HealthBar
+  init(name: String,life: Int, cost:Int){
+    let texture = SKTexture(imageNamed: name)
+    self.life = life
+    self.cost = cost
+    self.hb = HealthBar()
+    super.init(texture: texture, color: UIColor.clear, size: texture.size())
+    self.name = name
+  }
+  
+  required init?(coder aDecoder: NSCoder){
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func unitPlacement(_ blocks:Array<Block>,_ player:Player,_ scene:SKScene,_ node:SKNode, _ index:Int, _ grid:Grid,_ name:String, _ units:Array<Unit>)-> (Array<Block>,Array<Unit>){
+    var blocks = blocks
+    var units = units
+    let block = blocks[index]
+    var unit:Unit
+    //print(block)
+    if block.isFree == true && block.unit_name != name{
+      if !player.buyUnit(cost: cost){
+        print("not enough money !")
+      }
+      else{
+        blocks[index].isFree = false
+        blocks[index].unit_name = name
+        self.position = grid.gridPosition(row: block.coor.row+1, col: block.coor.col+1)
+        self.hb.position = CGPoint(x:self.position.x-30,y:self.position.y-30)
+        // adding bar here
+        //print(self.position)
+        scene.addChild(self)
+        scene.addChild(self.hb)
+        units.append(self)
+      }
+    }
+    else if block.isFree == false && block.unit_name == name{
+        //node.removeFromParent()
+        blocks[index].isFree = true
+        blocks[index].unit_name = ""
+        player.addMoney(self.cost)
+        unit = getRightUnit(grid.gridPosition(row: block.coor.row+1, col: block.coor.col+1),units)
+        unit.hb.removeFromParent()
+        unit.removeFromParent()
+        units.remove(at:units.index(of:unit)!)
+    }
+    else{
+      print("block is not free block unit_name ==",block.unit_name)
+    }
+    return (blocks,units)
+  }
+}
